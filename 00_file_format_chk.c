add3b3383a53f86e9732b0ad3515fc16481dfb27
Hoyoung Song <hoysong@c3r20s5.42gyeongsan.kr>
2025-06-14 02:44:56 +0900

    방향별 체크하는 로직 잘 작동하는 듯?

--- 00_file_format_chk.c
+++ 00_file_format_chk.c
@@ -27,16 +27,16 @@ static int wall_vld_chk(char *direction, char *wall_info)
 	char	**splits;
 
 	//direction check.
-	printf("WALL VLD CHECK\n");
-	printf("%s\n", direction);
-	printf("%s\n", wall_info);
+	printf("==== WALL VLD CHECK ====\n");
+	printf("direction: %s\n", direction);
+	printf("full line: %s\n", wall_info);
 	splits = ft_split(wall_info, ' ');
 	if (count_index(splits) != 2)
 	{
 		printf("not a 2 arguments\n");
 		return (0);
 	}
-	printf("\n");
+	printf("there is two argument. good!\n");
 	if (ft_strncmp(splits[0], direction, 3))
 	{
 		printf("direction not match..\n");
@@ -45,21 +45,32 @@ static int wall_vld_chk(char *direction, char *wall_info)
 		return (0);
 	}
 	del_newline(splits[1]);
+	printf("try open file: %s\n", splits[1]);
 	if (!try_open(splits[1]))
+	{
+		printf("try open fail..\n");
 		return (0);
+	}
+	printf("open success!\n");
 	free_splits(splits);
 	printf("good line!\n");
 	return (1);
 }
 
 /*This function returns 1 if content is not valid.*/
-static int	content_vld_chk(t_dnode *file_content)
+static int	file_content_vld_chk(t_dnode *file_content)
 {
-	/*NORTH CHECK*/
-	wall_vld_chk("NO", file_content->data);
-	/*SOUTH CHECK*/
-	/*WEST CHECK*/
-	/*EAST CHECK*/
+	if (wall_vld_chk("NO", file_content->data))
+		return (0);
+	file_content = file_content->next_node;
+	if (wall_vld_chk("SO", file_content->data))
+		return (0);
+	file_content = file_content->next_node;
+	if (wall_vld_chk("WE", file_content->data))
+		return (0);
+	file_content = file_content->next_node;
+	if (wall_vld_chk("EA", file_content->data))
+		return (0);
 	return (1);
 }
 
@@ -90,11 +101,12 @@ static int	try_open(char *file_name)
 
 	fd = open(file_name, O_RDONLY);
 	if (fd < 0)
-		return (0);
 	{
+		return (0);
 		printf("%s: open fail\n", file_name);
-	close(fd);
 	}
+	close(fd);
+	printf("%s: open success\n", file_name);
 	return (1);
 }
 
@@ -108,15 +120,16 @@ int	pars_map_vld_chk(int argc, char **argv)
 		return (1);
 	}
 	argv ++;
-	printf("Is this .cub file?\n");
+	printf("==== FORMAT(.cub) CHECK ====\n");
 	if (!is_vld_file_format(*argv, ".cub"))
 	{
 		printf("not a .cub file...\n");
 		return (1);
 	}
-	else if (!try_open(*argv))
+	printf("good format!\n");
+	if (!try_open(*argv))
 	{
-		printf(".cub open fail..\n");
+		printf("bad format..\n");
 		return (1);
 	}
 	fd = open(*argv, O_RDONLY);
@@ -124,7 +137,7 @@ int	pars_map_vld_chk(int argc, char **argv)
         // maybe get gnl lst first.
 	file_content = get_gnl_node(fd);
 	file_content = file_content->next_node;
-	if (!content_vld_chk(file_content))
+	if (!file_content_vld_chk(file_content))
 	{
 		return (1);
 	}
