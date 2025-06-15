664357b7d0e43d2da0c6dd6932f39e4380cf2a85
Hoyoung Song <hoysong@c3r20s2.42gyeongsan.kr>
2025-06-16 06:25:16 +0900

    restart parser

--- prev_00/00_map_vld_chk.c
+++ prev_00/00_map_vld_chk.c
@@ -12,8 +12,8 @@ static int	count_index(char **splits)
 	return (i);
 }
 
-static int	is_vld_file_format(char *file_name, char *file_format);
-static int	try_open(char *file_name);
+extern int	is_vld_file_format(char *file_name, char *file_format);
+extern int	try_open(char *file_name);
 
 static void	del_newline(char *str)
 {
@@ -199,36 +199,6 @@ static int	file_content_vld_chk(t_dnode *file_content)
 	file_content = file_content->next_node;
 	/*CLOSED MAP CHECK.*/
 	is_surrounded(file_content);
-	return (1);
-}
-
-static int	is_vld_file_format(char *file_name, char *file_format)
-{
-	int	i;
-
-	i = 0;
-	while (file_name[i])
-		i++;
-	while (file_name[i] != '.')
-		i--;
-	if (ft_strncmp(&(file_name[i]), file_format, ft_strlen(file_format) + 1))
-		return (0);
-	return (1);
-}
-
-/*this function returns 0 if open fail.*/
-static int	try_open(char *file_name)
-{
-	int	fd;
-
-	fd = open(file_name, O_RDONLY);
-	if (fd < 0)
-	{
-		return (0);
-		printf("%s: open fail\n", file_name);
-	}
-	close(fd);
-	printf("%s: open success\n", file_name);
 	return (1);
 }
 
--- prev_00/compile.sh
+++ prev_00/compile.sh
@@ -1,5 +1,6 @@
 FLAG="-Wall -Wextra -Werror -g"
 cc $FLAG \
 	./main.c \
-	./00_file_format_chk.c \
+	./00_map_vld_chk.c \
+	./01_is_vld_file_format.c \
 	./my_libft/libft.a
