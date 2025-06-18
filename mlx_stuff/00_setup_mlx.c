908cca85960ea21e85c4dd9671a97f1d312319cc
hoysong(Lionine) <eagleteo@naver.com>
2025-06-19 07:12:49 +0900

    destroy notify added

--- mlx_stuff/00_setup_mlx.c
+++ mlx_stuff/00_setup_mlx.c
@@ -3,12 +3,16 @@
 #include "../minilibx-linux/mlx.h"
 #include <stdlib.h>
 
+static int destroy_notify_hook(void *hi)
+{
+	mlx_loop_end(mlx()->mlx_ptr);
+	return (0);
+}
+
 static int	hook_func(int key_input)
 {
 	if (key_input == XK_Escape)
-	{
 		mlx_loop_end(mlx()->mlx_ptr);
-	}
 	return (0);
 }
 
@@ -38,6 +42,9 @@ int	setup_mlx(void)
 	mlx->mlx_window = mlx_new_window(mlx->mlx_ptr, WIN_WIDTH, WIN_HEIGHT, get_pars()->argv[1]);
 	make_floor_ceiling_image();
 
+	/*ESC hook.*/
 	mlx_hook(mlx->mlx_window, KeyPress, KeyPressMask, hook_func, mlx);
+	/*Destroy('x' button in window) hook.*/
+	mlx_hook(mlx->mlx_window, DestroyNotify, NoEventMask, destroy_notify_hook, mlx);
 	return (1);
 }
