cc2d09d060d7fdfaf56ff7ae52b54f4323e2fe53
hoysong(Lionine) <eagleteo@naver.com>
2025-06-21 19:38:00 +0900

    일부 주석 영어로.

--- mlx_stuff/cub_defs.h
+++ mlx_stuff/cub_defs.h
@@ -1,20 +1,24 @@
 #ifndef CUB_DEFS_H
 # define CUB_DEFS_H
-/*플레이어의 시야각.*/
+
+/*Window size.*/
+# define WIN_WIDTH	900
+# define WIN_HEIGHT	900
+
+/*Minimap size.*/
+# define MINISIZE	900
+/*Minimap BG_color.*/
+# define BG_Color	0xdddddd
+/*Minimap GridColor.*/
+# define GridColor	0xdddddd
+/*Minimap WallColor.*/
+# define WallColor	0x999999
+
+/*Player FOV.*/
 # define Player_FOV	90
 /*플레이어의 가시반경*/
 # define VIS_RADIUS	3
-/*레이캐스팅 시 해상각도.*/
+/*레이캐스팅 투사 각도.*/
 # define RAY_RES	3
-/*윈도우의 사이즈*/
-# define WIN_WIDTH	900
-# define WIN_HEIGHT	900
-/*미니맵 사이즈.*/
-# define MINISIZE	900
-/*미니맵 배경 색상.*/
-# define BG_Color	0xdddddd
-/*미니맵 그리드 색상.*/
-# define GridColor	0xdddddd
-/*미니맵 벽 색상.*/
-# define WallColor	0x999999
+
 #endif
