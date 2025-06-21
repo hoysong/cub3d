5a96b6fb51844af160949d7fe58a71731bc66440
hoysong(Lionine) <eagleteo@naver.com>
2025-06-21 10:51:18 +0900

    테스트로 90도 각도로 시야범위를 주고 해봤다. 일단 시야를 보는 위치에 따라 시작을 해야 하는데 어떡하지.

--- mlx_stuff/04_make_minimap.c
+++ mlx_stuff/04_make_minimap.c
@@ -122,10 +122,18 @@ void	make_minimap(void)
 	/*test...*/
 	int i = 1;
 
-	while (i < 5)
+	while (i < 45)
 	{
 		get_dot(player()->cord, player()->view_point, i);
 		i++;
+	}
+
+	i = 1;
+
+	while (i > -45)
+	{
+		get_dot(player()->cord, player()->view_point, i);
+		i--;
 	}
 
 }
--- mlx_stuff/algorithm/01_visible_radius.c
+++ mlx_stuff/algorithm/01_visible_radius.c
@@ -9,20 +9,12 @@ void	get_dot(t_point me, t_point vp, float degree)
 {
 	t_point	new;
 
-	//vp.x -= me.x;
-	//vp.y -= me.y;
-	//printf("vpx: %f vpy: %f\n", vp.x, vp.y);
-	//vp.x = vp.x * (float)cos(degree) - vp.y * (float)sin(degree);
-	//vp.y = vp.x * (float)sin(degree) + vp.y * (float)cos(degree);
-	//vp.x += me.x;
-	//vp.y += me.y;
 	printf("%f %f\n", vp.x, vp.y);
 	new.x = ((vp.x - me.x)*(float)cos(degree * (Pie/180)) - (vp.y - me.y)*(float)sin(degree *(Pie/180))) + me.x;
 	new.y = ((vp.x - me.x)*(float)sin(degree * (Pie/180)) + (vp.y - me.y)*(float)cos(degree *(Pie/180))) + me.y;
 	printf("degree:%f | vp: x: %f, y: %f\n", degree, new.x, new.y);
 	/*test put_pixel*/
-	put_pixel_to_img(&(mlx()->minimap), new.x * (float)get_minimap_ratio(), new.y * (float)get_minimap_ratio(), 0x0);
-	put_pixel_to_img(&(mlx()->minimap), 1 + new.x * (float)get_minimap_ratio(), 1 + new.y * (float)get_minimap_ratio(), 0x0);
+	put_pixel_to_img(&(mlx()->minimap), new.x * (float)get_minimap_ratio(), new.y * (float)get_minimap_ratio(), 0xff0000);
 }
 
 //void	get_visivle_radius(void)
