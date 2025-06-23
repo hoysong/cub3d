c0ecf6f3b7de2de34bc5fad6702dfa2da5526511
hoysong(Lionine) <eagleteo@naver.com>
2025-06-23 14:09:18 +0900

    Revert "LEAK valgrind 구동 시에 leak이 발생하나 fsanitize에서는 발생하지 않는다."
    This reverts commit 356c6d45233171b2e18ca4bb6b8b087ec37adce5.

--- mlx_stuff/cub_defs.h
+++ mlx_stuff/cub_defs.h
@@ -17,7 +17,7 @@
 # define FOV_COLOR	0xff0000
 
 /*size of block.*/
-# define SIZE_OF_BLOCK	10
+# define SIZE_OF_BLOCK	100
 
 /*플레이어의 가시반경.*/
 /*블록의 크기 * n 만큼이 지정되도록 함.*/
@@ -27,9 +27,9 @@
 # define DEPTH			3
 
 /*Player FOV.*/
-# define Player_FOV		65
+# define Player_FOV		90
 /*Player Angle per tick.*/
-# define CAM_ROTATE_ANGLE	7
+# define CAM_ROTATE_ANGLE	1
 /*Player's move distance.*/
 # define STEP_PER_PIXEL	5
 /*Convert Player cordinate to minimap_res*/
