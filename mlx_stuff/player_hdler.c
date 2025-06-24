a6ca79d790fc64bdfb931de36fa50dfc2a130034
hoysong(Lionine) <eagleteo@naver.com>
2025-06-24 10:19:27 +0900

    설마설마 했는데 -를 곱해주니 되었다.

--- mlx_stuff/player_hdler.c
+++ mlx_stuff/player_hdler.c
@@ -28,7 +28,6 @@ void	player_get_step_point(t_player *player)
 void	player_rotate(t_player *player, float degree)
 {
 	player->view_point = rotate_point(player->cord, player->view_point, degree);
-	player_get_step_point(player);
 }
 
 void	player_move(t_player *player)
@@ -40,8 +39,12 @@ void	player_move(t_player *player)
 	printf("player step: x=%f | y=%f\n",
 			player->step_point.x,
 			player->step_point.y);
-	player->view_point.x += player->cord.x - player->step_point.x;
-	player->view_point.y += player->cord.y - player->step_point.y;
+	printf("calculate  : x=%f | y=%f\n",
+			player->step_point.x - player->cord.x,
+			player->step_point.y - player->cord.y
+	);
+	player->view_point.x += -(player->cord.x - player->step_point.x);
+	player->view_point.y += -(player->cord.y - player->step_point.y);
 	player->cord.x = player->step_point.x;
 	player->cord.y = player->step_point.y;
 	printf("after player_cord: x=%f | y=%f\n", player->cord.x, player->cord.y);
