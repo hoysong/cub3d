80472ed61e7b508065f914409f51168b001fb5ed
hoysong(Lionine) <eagleteo@naver.com>
2025-06-25 18:44:11 +0900

    ToDo... 하루죙일 둥글던 벽이.. 선분의 직선으로 만나는 위치를 계산하여 직각거리를 구해서 어느정도 해결된 느낌인데 완벽히 해결되진 않았다. 뭘 더 할 수 있는지 모르겠다.
    1. 뷰포인트 기준으로 모든걸 계산하는 문제.
    거리에 따라서 벽의 모양이 직사각형이나 정사각형이 되어버리는 문제가 있다.
    아마도 다른 문제도 여기서 발생할 수 있다.
    텍스쳐가 갑자기 늘어났다 줄어들었다 한다고 생각을 해보면 매우 이상하다.
    일관성 있는 사각형을 만들어내야 한다.
    
    2. 뭐 할 수 있지

--- mlx_stuff/algorithm/ray_cast.c
+++ mlx_stuff/algorithm/ray_cast.c
@@ -1,6 +1,13 @@
 #include "../player.h"
 #include <math.h>
-//#include <stdio.h>
+#include <stdio.h>
+
+static inline float	my_abs(float num)
+{
+	if (num < 0)
+		return (num *= -1);
+	return (num);
+}
 
 /*return's length of between p1 and p2.*/
 static inline float	get_length(t_point p1, t_point p2)
@@ -8,19 +15,38 @@ static inline float	get_length(t_point p1, t_point p2)
 	return (sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2)));
 }
 
-static void	ray_casting(t_mlx *mlx, float degree, t_ray_info *info)
+static inline float	get_win_x(float degree)
 {
-	float percent_of_degree;
+	float	percent_of_degree;
+
+	percent_of_degree = (degree / Player_FOV) * 100;
+	return ((float)WIN_WIDTH * percent_of_degree / 100);
+}
+
+static float	get_len(t_point a, t_point b, t_point c)
+{
+	float	A = b.y - a.y;
+	float	B = -(b.x - a.x);
+	float	C = -a.x*(b.y - a.y) + a.y*(b.x - a.x);
+
+	float	len;
+	len = my_abs(A*c.x + B*c.y + C) / sqrt(pow(A, 2) + pow(B, 2));
+	return (len);
+}
+static void	ray_casting_prev(t_mlx *mlx, float degree, t_ray_info *info)
+{
+	float	win_x;
 
 	/*각도를 변환하여 X의 위치에 점을 찍기.*/
 		/*각도가 0부터 시작하도록 변경.*/
 		degree += (float)Player_FOV / 2;
+		win_x = get_win_x(degree);
 		/*레이 각도마다 일단 점을 찍어본다.*/
-		percent_of_degree = (degree / Player_FOV) * 100;
-		put_pixel_to_img(&(mlx->background), (float)WIN_WIDTH * percent_of_degree / 100, WIN_HEIGHT / 2, 0x0);
+		//put_pixel_to_img(&(mlx->background), (float)WIN_WIDTH * percent_of_degree / 100, WIN_HEIGHT / 2, 0x0);
 	/*플레이어와 벽충돌점을 시야최대거리로부터 퍼센트화.*/
 		float	view_dist = get_length(player()->cord, player()->view_point);;
-		float	ray_hit_dist = get_length(player()->cord, info->ray_hit);
+		float	ray_hit_dist;
+		ray_hit_dist = get_length(player()->cord, info->ray_hit);
 		float	percent_of_hit_dist = (ray_hit_dist / view_dist) * 100;
 		float	vert_from_up = ((float)WIN_HEIGHT * percent_of_hit_dist / 100) / 2; // 선은 위에서부터 긋는다. 시작지점.
 		float	vert_from_down = (float)WIN_HEIGHT - vert_from_up; // 그어진 선이 아래에 도착하는 도착지점.
@@ -28,7 +54,48 @@ static void	ray_casting(t_mlx *mlx, float degree, t_ray_info *info)
 		int	i = vert_from_up;
 		while (i < vert_from_down)
 		{
-			put_pixel_to_img(&(mlx->background), (float)WIN_WIDTH * percent_of_degree / 100, i, 0xff0000);
+			put_pixel_to_img(&(mlx->background), win_x, i, 0xff0000);
+			i++;
+		}
+}
+
+static void	ray_casting(t_mlx *mlx, float degree, t_ray_info *info)
+{
+	float	win_x;
+
+	/*각도를 변환하여 X의 위치에 점을 찍기.*/
+		/*각도가 0부터 시작하도록 변경.*/
+		degree += (float)Player_FOV / 2;
+		win_x = get_win_x(degree);
+		/*레이 각도마다 일단 점을 찍어본다.*/
+		//put_pixel_to_img(&(mlx->background), (float)WIN_WIDTH * percent_of_degree / 100, WIN_HEIGHT / 2, 0x0);
+	/*플레이어와 벽충돌점을 시야최대거리로부터 퍼센트화.*/
+		float	view_dist = get_length(player()->cord, player()->view_point);;
+		float	ray_hit_dist;
+		//if ((90 - (float)Player_FOV/2) + degree < 90)
+		//{
+		//	printf("degree: %f\n", degree);
+			ray_hit_dist = get_len(
+				rotate_point(player()->cord, player()->view_point, -90),
+				player()->cord,
+				info->ray_hit
+				);
+//				printf("hit_dist: %f\n", ray_hit_dist);
+//				printf("%f | %f\n\n",
+//						rotate_point(player()->cord, player()->view_point, -90).x,
+//						rotate_point(player()->cord, player()->view_point, -90).y
+//						);
+		//}
+		//else
+		//	ray_hit_dist = get_length(player()->cord, info->ray_hit);
+		float	percent_of_hit_dist = (ray_hit_dist / view_dist) * 100;
+		float	vert_from_up = ((float)WIN_HEIGHT * percent_of_hit_dist / 100) / 2; // 선은 위에서부터 긋는다. 시작지점.
+		float	vert_from_down = (float)WIN_HEIGHT - vert_from_up; // 그어진 선이 아래에 도착하는 도착지점.
+
+		int	i = vert_from_up;
+		while (i < vert_from_down)
+		{
+			put_pixel_to_img(&(mlx->background), win_x, i, 0x0000);
 			i++;
 		}
 }
@@ -51,6 +118,7 @@ void	shoot_fov_ray(void)
 		if (shoot_ray(start, dest, &(ray_info), detect_wall_hit))
 		{
 			ray_casting(mlx(), degree, &(ray_info));
+			ray_casting_prev(mlx(), degree, &(ray_info));
 		}
 		degree += RAY_RES;
 	}
