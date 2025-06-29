24bea85bed5c1d84f90b154605486e6294773320
hoysong(Lionine) <eagleteo@naver.com>
2025-06-29 20:28:27 +0900

    코드 정리됨.

--- mlx_stuff/algorithm/ray_cast.c
+++ mlx_stuff/algorithm/ray_cast.c
@@ -36,9 +36,6 @@ static inline float	unfisheye_len(t_point a, t_point b, t_point c)
 
 static void	ray_casting(t_mlx *mlx, float degree, t_ray_info *info)
 {
-	float	win_x;
-	degree += (float)Player_FOV / 2;
-	win_x = get_win_x(degree);
 	float	view_dist = get_length(player()->cord, player()->view_point);;
 	float	perp_wall_dist;
 	perp_wall_dist = unfisheye_len(
@@ -46,42 +43,20 @@ static void	ray_casting(t_mlx *mlx, float degree, t_ray_info *info)
 		player()->cord,
 		info->ray_hit
 		);
-	///*윈도우 비율로 맞추는 과정.*/
-	//	/*어안렌즈 보정 값은 뷰포인트의 어느정도에 해당하는가?*/
-	//	float	percent_view_dist = (perp_wall_dist / view_dist) * 100;
-	//	/*최대가시거리와 윈도우의 비례식?*/
-	//	float	brsick = view_dist * WIN_HEIGHT;
-	//	/**/
-	//	float	rbrsick_wall_height = brsick / perp_wall_dist;
-	//	/**/
-	//	float	real_height = rbrsick_wall_height / WIN_HEIGHT * 100;
-
-	//	/*이미지의 시작과 끝점 지정.*/
-	//	float	vert_start = (WIN_HEIGHT - real_height)/2;
-	//	float	vert_end = (float)WIN_HEIGHT / 2 + real_height / 2;
-	//	printf("%f\n", real_height);
-
-	//	int	i = vert_start;
-	//	while (i < vert_end)
-	//	{
-	//		put_pixel_to_img(&(mlx->background), win_x, i, 0x0000);
-	//		i++;
-	//	}
 	/*2번째 코드.*/
-//	/*윈도우 비율로 맞추는 과정.*/
-	/*어안렌즈 보정 값은 뷰포인트의 어느정도에 해당하는가?*/
-	/*100 퍼센트 중 어느정도에 해당하는가?*/
+	/*윈도우 비율로 맞추는 과정.*/
 	float	percent_view_dist = (perp_wall_dist / view_dist) * 100;
 	/*100퍼센트에서 해당하는 만큼을 다시 빼서 역수로 만든다.*/
 	float	rev_per = 100 - percent_view_dist;
 	/*역수를 다시 Window 높이에 적용한다.*/
 	float	win_height_len = WIN_HEIGHT * ((rev_per + 17) / 100);
-	printf("view_point_len : %f\n", get_length(player()->cord, player()->view_point));
-	printf("rev_per        : %f\n", rev_per);
-	printf("win_height_len : %f\n", win_height_len);
 	int	i = (WIN_HEIGHT - win_height_len) / 2;
 	printf("%d\n", i);
 	win_height_len += i;
+	/*get_x*/
+	float	win_x;
+	degree += (float)Player_FOV / 2;
+	win_x = get_win_x(degree);
 	while (i < win_height_len)
 	{
 		put_pixel_to_img(&(mlx->background), win_x, i, 0x0000);
