9ad989b4bb8d414110581c1b8e7ea1c9c867876c
hoysong(Lionine) <eagleteo@naver.com>
2025-07-14 02:38:00 +0900

    ToDo. 코사인 각도 관련 핸들링이 굉장히 미숙했으나 다른 공식으로 대체하였다. 3점이 있을 경우, 선분 ab를 c가 수직으로 만나는 거리를 구하는 공식으로 각도조차 필요하지 않다. 350도까지 테스트를 해봤으며 당장은 문제가 안보인다. 아울러 이전에 사용하던 부분(코사인+각도를 통해 수직거리를 구하는 방식)은 주석되어있다.
    1. 테스트 더 해볼 것.
    2. 값이 일정한지 확인할 것.
    3. 코드를 정리할 것.

--- mlx_stuff/algorithm/ray_cast.c
+++ mlx_stuff/algorithm/ray_cast.c
@@ -1,11 +1,22 @@
 #include "../player.h"
 #include "../mlx_hdler.h"
 #include <math.h>
+#include <stdio.h>
 
 static inline float	zero_start_degree(float degree)
 {
 	degree += (float)Player_FOV / 2;
 	return (degree);
+}
+
+float	get_vertlen(t_point a, t_point b, t_point c)
+{
+	float	m;
+	m = (b.y - a.y) / (b.x - a.x);
+
+	float result;
+	result = my_abs(m*c.x - c.y + (a.y - (m*a.x))) / sqrt(pow(m, 2) + 1);
+	return (result);
 }
 
 static inline float	get_vertical_length(float degree, t_ray_info *info)
@@ -20,13 +31,33 @@ static inline float	get_vertical_length(float degree, t_ray_info *info)
 	float	hit_len;
 	float	perp_len;
 
-	hit_len = get_length(player()->cord, info->ray_hit);
-	perp_len = cos(cos_degree * (Pie/180)) * hit_len;
+	hit_len = get_vertlen(
+			player()->cord,
+			rotate_point(player()->cord, player()->view_point, 90),
+			info->ray_hit);
+	perp_len = hit_len;
+
+//	hit_len = get_length(player()->cord, info->ray_hit);
+//	/*90도인 경우 0이 나와버려 문제가 발생한다 (주석 없애지 말 것.)*/
+//	/*지금 해결된 상태는 아님.*/
+//	perp_len = cos(cos_degree * (Pie/180)) * hit_len;
+
 	/*to inverse*/
 	float	inverse;
 
+	//if (cos_degree == 90)
+	//{
+	//	perp_len = hit_len;
+	//}
 	inverse = (SIZE_OF_BLOCK * WIN_HEIGHT) / perp_len;
-	if (inverse > WIN_HEIGHT)
+	//printf("degree  : %f\n", degree);
+	//printf("cos_degree  : %f\n", cos_degree);
+	printf("rayhit  : %f\n", hit_len);
+	//printf("cos()   : %f\n", cos(cos_degree * (Pie/180)));
+	printf("cos*hit : %f\n", perp_len);
+	printf("RESULT  : %f\n", inverse);
+	printf("\n");
+	if (inverse > WIN_HEIGHT || inverse < 0)
 		inverse = WIN_HEIGHT;
 	return (inverse);
 }
