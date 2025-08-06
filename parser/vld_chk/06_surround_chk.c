d978afec8105ff93e0a602afb9ebe3cfdf085fa4
hoysong(Lionine) <eagleteo@naver.com>
2025-08-06 19:25:57 +0900

    player가 없을 경우 문제가 발생하던 상황을 해결.

--- parser/vld_chk/06_surround_chk.c
+++ parser/vld_chk/06_surround_chk.c
@@ -37,10 +37,15 @@ static int	diagonal_vld_check(char **map, int i, int j)
 static int	is_surrounded_by_wall_floor(char **map, int i, int j)
 {
 	if (
-		is_wall_floor(map[i - 1][j]) &&
-		is_wall_floor(map[i + 1][j]) &&
-		is_wall_floor(map[i][j - 1]) &&
-		is_wall_floor(map[i][j + 1])
+		map[i - 1][j - 1] != ' ' &&
+		map[i - 1][j + 1] != ' ' &&
+		map[i + 1][j - 1] != ' ' &&
+		map[i + 1][j + 1] != ' ' &&
+		diagonal_vld_check(map, i, j)
+		//is_wall_floor(map[i - 1][j]) &&
+		//is_wall_floor(map[i + 1][j]) &&
+		//is_wall_floor(map[i][j - 1]) &&
+		//is_wall_floor(map[i][j + 1])
 		)
 	{
 		return (1);
@@ -53,18 +58,20 @@ static int	surround_chk(char **map, int i, int j)
 	if (map[i][j] == ' ' || map[i][j] == '1')
 		return (1);
 	if (is_player(map[i][j]) &&
-		is_surrounded_by_wall_floor(map, i, j) &&
-		diagonal_vld_check(map, i, j)
+		is_surrounded_by_wall_floor(map, i, j)
+//		is_surrounded_by_wall_floor(map, i, j) &&
+//		diagonal_vld_check(map, i, j)
 		)
 	{
 		return (1);
 	}
 	else if (map[i][j] == '0' &&
-			(is_player(map[i - 1][j]) || is_wall_floor(map[i - 1][j])) &&
-			(is_player(map[i + 1][j]) || is_wall_floor(map[i + 1][j])) &&
-			(is_player(map[i][j - 1]) || is_wall_floor(map[i][j - 1])) &&
-			(is_player(map[i][j + 1]) || is_wall_floor(map[i][j + 1])) &&
-			diagonal_vld_check(map, i, j)
+			is_surrounded_by_wall_floor(map, i, j)
+//			(is_player(map[i - 1][j]) || is_wall_floor(map[i - 1][j])) &&
+//			(is_player(map[i + 1][j]) || is_wall_floor(map[i + 1][j])) &&
+//			(is_player(map[i][j - 1]) || is_wall_floor(map[i][j - 1])) &&
+//			(is_player(map[i][j + 1]) || is_wall_floor(map[i][j + 1])) &&
+//			diagonal_vld_check(map, i, j)
 			)
 	{
 		return (1);
@@ -94,6 +101,6 @@ void	good_map_chk(char **map)
 		j = 1;
 		i++;
 	}
-	if (player_count > 1)
-		get_pars()->pars_errno = MULTIPLE_PLAYER;
+	if (player_count != 1)
+		get_pars()->pars_errno = 1;
 }
